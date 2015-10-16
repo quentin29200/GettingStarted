c2e58125c7084799370ed1f3aae3eee882fc0b19
quentin29200 <quentin.lgdc@gmail.com>
2015-10-16 09:28:05 +0200

    Squelette HTMLExportCustom

---
+++ src/main/java/org/opencompare/HTMLExporterCustom.java
@@ -0,0 +1,97 @@
+package org.opencompare;
+
+import org.opencompare.api.java.*;
+import org.opencompare.api.java.io.HTMLExporter;
+import org.opencompare.api.java.value.*;
+
+
+public class HTMLExporterCustom extends HTMLExporter {
+    public HTMLExporterCustom() {
+
+    }
+
+    public String export(PCMContainer container) {
+        return null;
+    }
+
+    public String toHTML(PCM pcm) {
+        return null;
+    }
+
+    public String toHTML(PCMContainer container) {
+        return null;
+    }
+
+    public void visit(PCM pcm) {
+
+    }
+
+    public void visit(Feature feature) {
+
+    }
+
+    public void visit(FeatureGroup featureGroup) {
+
+    }
+
+    public void visit(Product product) {
+
+    }
+
+    public void visit(Cell cell) {
+
+    }
+
+    @Override
+    public void visit(BooleanValue booleanValue) {
+
+    }
+
+    public void visit(Conditional conditional) {
+
+    }
+
+    public void visit(DateValue dateValue) {
+
+    }
+
+    public void visit(Dimension dimension) {
+
+    }
+
+    public void visit(IntegerValue integerValue) {
+
+    }
+
+    public void visit(Multiple multiple) {
+
+    }
+
+    public void visit(NotApplicable notApplicable) {
+
+    }
+
+    public void visit(NotAvailable notAvailable) {
+
+    }
+
+    public void visit(Partial partial) {
+
+    }
+
+    public void visit(RealValue realValue) {
+
+    }
+
+    public void visit(StringValue stringValue) {
+
+    }
+
+    public void visit(Unit unit) {
+
+    }
+
+    public void visit(Version version) {
+
+    }
+}
