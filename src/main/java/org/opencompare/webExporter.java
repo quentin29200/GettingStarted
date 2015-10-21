430ad71ba88719ef152926a953cc0ea3074ff8d2
cheisda <cheisda@gmail.com>
2015-10-21 11:36:09 +0200

    création webExporter commitée sur branche developp

---
+++ src/main/java/org/opencompare/webExporter.java
@@ -0,0 +1,10 @@
+package org.opencompare;
+
+import org.opencompare.api.java.util.PCMVisitor;
+
+/**
+ * Created by Cheisda on 21/10/2015.
+ */
+public abstract class webExporter implements PCMVisitor{
+
+}
