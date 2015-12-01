5a1c4f32e5d7ac9f86917064d00d78e4c0ae5543
cheisda <cheisda@gmail.com>
2015-12-01 14:58:45 +0100

    Javadoc

--- src/main/java/org/opencompare/HTMLExporterCustom.java
+++ src/main/java/org/opencompare/HTMLExporterCustom.java
@@ -10,6 +10,7 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -47,7 +48,6 @@ public class HTMLExporterCustom extends HTMLExporter {
     private Param parameters;
 
     /**
-     *
      * @return parameters
      */
     public Param getParameters() {
@@ -55,7 +55,6 @@ public class HTMLExporterCustom extends HTMLExporter {
     }
 
     /**
-     *
      * @param parameters
      */
     public void setParameters(Param parameters) {
@@ -65,7 +64,6 @@ public class HTMLExporterCustom extends HTMLExporter {
     //constructeur de la classe
 
     /**
-     *
      * @param fileName
      */
     public HTMLExporterCustom(String fileName) {
@@ -74,7 +72,6 @@ public class HTMLExporterCustom extends HTMLExporter {
     }
 
     /**
-     *
      * @param container
      * @return
      */
@@ -83,7 +80,6 @@ public class HTMLExporterCustom extends HTMLExporter {
     }
 
     /**
-     *
      * @param pcm
      * @return
      */
@@ -92,7 +88,7 @@ public class HTMLExporterCustom extends HTMLExporter {
         this.doc = Jsoup.parse(this.templateFull);
         this.body = this.doc.body();
         this.doc.head().select("title").first().text(pcm.getName());
-        if(this.metadata == null) {
+        if (this.metadata == null) {
             this.metadata = new PCMMetadata(pcm);
         }
 
@@ -101,10 +97,8 @@ public class HTMLExporterCustom extends HTMLExporter {
     }
 
     /**
-     *
      * @param container
      * @return contains
-     *
      */
     public String toHTML(PCMContainer container) {
         this.metadata = container.getMetadata();
@@ -112,7 +106,6 @@ public class HTMLExporterCustom extends HTMLExporter {
     }
 
     /**
-     *
      * @param pcm
      */
     public void visit(PCM pcm) {
@@ -133,7 +126,7 @@ public class HTMLExporterCustom extends HTMLExporter {
         Iterator<DataStyle> itParam = parameters.getDataStyleParam().iterator();
         String name = null;
 
-        while(var5.hasNext() || itParam.hasNext()) {
+        while (var5.hasNext() || itParam.hasNext()) {
             /*Product var7 = (Product)var5.next();
             this.tr = table.appendElement("tr");
             var7.accept(this);*/
@@ -142,28 +135,28 @@ public class HTMLExporterCustom extends HTMLExporter {
             Collections.sort(cells, new Comparator() {
                 @Override
                 public int compare(Object o1, Object o2) {
-                    Cell cell1 = (Cell)o1;
-                    Cell cell2 = (Cell)o2;
+                    Cell cell1 = (Cell) o1;
+                    Cell cell2 = (Cell) o2;
                     return HTMLExporterCustom.this.metadata.getSortedFeatures().indexOf(cell1.getFeature()) - HTMLExporterCustom.this.metadata.getSortedFeatures().indexOf(cell2.getFeature());
 
                 }
             });
             Iterator var3 = cells.iterator();
             DataStyle ds = null;
-            if(itParam.hasNext()){
+            if (itParam.hasNext()) {
                 ds = itParam.next();
 
             }
             this.tr = table.appendElement("tr");
             this.tr.appendElement("th").text(var7.getName());
             //Iterator<Cell> var8 = var7.getCells().iterator();
-            while(var3.hasNext()) {
-                Cell cell = (Cell)var3.next();
+            while (var3.hasNext()) {
+                Cell cell = (Cell) var3.next();
                 cell.getContent();
                 //System.out.println(cell);
                 // this.tr = table.appendElement("tr");
                 //** By Chloé hu
-                if(itParam.hasNext()){
+                if (itParam.hasNext()) {
                     name = ds.getName();
                     if (name.contains("rangein")) { // A modifier, en haut mettre des booléens si rangein ou rangeout dans fichier Param
                         rangeIn(ds.getBorneinf(), ds.getBornesup(), ds.getValue());
@@ -180,19 +173,18 @@ public class HTMLExporterCustom extends HTMLExporter {
             }
         }
 
-        if(this.parameters.isShowBottomNameFeatures() && !this.parameters.isReversePCM()){
+        if (this.parameters.isShowBottomNameFeatures() && !this.parameters.isReversePCM()) {
             this.featuresRow(pcm, table);
         }
 
     }
 
     /**
-     *
      * @param feature
      */
     public void visit(Feature feature) {
         Element th = this.tr.appendElement("th");
-        if(this.featureDepth > 1) {
+        if (this.featureDepth > 1) {
             th.attr("rowspan", Integer.toString(this.featureDepth));
         }
 
@@ -200,12 +192,11 @@ public class HTMLExporterCustom extends HTMLExporter {
     }
 
     /**
-     *
      * @param featureGroup
      */
     public void visit(FeatureGroup featureGroup) {
         Element th = this.tr.appendElement("th");
-        if(!featureGroup.getFeatures().isEmpty()) {
+        if (!featureGroup.getFeatures().isEmpty()) {
             th.attr("colspan", Integer.toString(featureGroup.getFeatures().size()));
         }
 
@@ -223,8 +214,8 @@ public class HTMLExporterCustom extends HTMLExporter {
         Collections.sort(cells, new Comparator() {
             @Override
             public int compare(Object o1, Object o2) {
-                Cell cell1 = (Cell)o1;
-                Cell cell2 = (Cell)o2;
+                Cell cell1 = (Cell) o1;
+                Cell cell2 = (Cell) o2;
                 return HTMLExporterCustom.this.metadata.getSortedFeatures().indexOf(cell1.getFeature()) - HTMLExporterCustom.this.metadata.getSortedFeatures().indexOf(cell2.getFeature());
 
             }
@@ -235,8 +226,8 @@ public class HTMLExporterCustom extends HTMLExporter {
         });
         Iterator var3 = cells.iterator();
 
-        while(var3.hasNext()) {
-            Cell cell = (Cell)var3.next();
+        while (var3.hasNext()) {
+            Cell cell = (Cell) var3.next();
             Element td = this.tr.appendElement("td");
             td.appendElement("span").text(cell.getContent());
         }
@@ -244,7 +235,6 @@ public class HTMLExporterCustom extends HTMLExporter {
     }
 
     /**
-     *
      * @param product
      */
     public void visit(Product product) {
@@ -253,8 +243,8 @@ public class HTMLExporterCustom extends HTMLExporter {
         Collections.sort(cells, new Comparator() {
             @Override
             public int compare(Object o1, Object o2) {
-                Cell cell1 = (Cell)o1;
-                Cell cell2 = (Cell)o2;
+                Cell cell1 = (Cell) o1;
+                Cell cell2 = (Cell) o2;
                 return HTMLExporterCustom.this.metadata.getSortedFeatures().indexOf(cell1.getFeature()) - HTMLExporterCustom.this.metadata.getSortedFeatures().indexOf(cell2.getFeature());
             }
             /*public int compare(Cell cell1, Cell cell2) {
@@ -263,8 +253,8 @@ public class HTMLExporterCustom extends HTMLExporter {
         });
         Iterator var3 = cells.iterator();
 
-        while(var3.hasNext()) {
-            Cell cell = (Cell)var3.next();
+        while (var3.hasNext()) {
+            Cell cell = (Cell) var3.next();
             Element td = this.tr.appendElement("td");
             td.appendElement("span").text(cell.getContent());
         }
@@ -272,11 +262,10 @@ public class HTMLExporterCustom extends HTMLExporter {
     }
 
     /**
-     *
      * @param pcm
      * @param table
      */
-    public void featuresRow(PCM pcm, Element table){
+    public void featuresRow(PCM pcm, Element table) {
         // List of features
         LinkedList featuresToVisit = new LinkedList();
         this.nextFeaturesToVisit = new LinkedList();
@@ -285,12 +274,12 @@ public class HTMLExporterCustom extends HTMLExporter {
         this.tr = table.appendElement("tr");
         this.tr.appendElement("th").attr("rowspan", Integer.toString(this.featureDepth)).text("Product");
         Iterator var5;
-        while(!featuresToVisit.isEmpty()) {
+        while (!featuresToVisit.isEmpty()) {
             Collections.sort(featuresToVisit, new Comparator() {
                 @Override
                 public int compare(Object o1, Object o2) {
-                    AbstractFeature feat1 = (AbstractFeature)o1;
-                    AbstractFeature feat2 = (AbstractFeature)o2;
+                    AbstractFeature feat1 = (AbstractFeature) o1;
+                    AbstractFeature feat2 = (AbstractFeature) o2;
                     return HTMLExporterCustom.this.metadata.getFeaturePosition(feat1) - HTMLExporterCustom.this.metadata.getFeaturePosition(feat2);
                 }
                 /*public int compare(AbstractFeature feat1, AbstractFeature feat2) {
@@ -314,8 +303,8 @@ public class HTMLExporterCustom extends HTMLExporter {
                 // Demander si on doit mettre la classe dans un span plutôt que dans th
             }*/
 
-            while(var5.hasNext()) {
-                AbstractFeature product = (AbstractFeature)var5.next();
+            while (var5.hasNext()) {
+                AbstractFeature product = (AbstractFeature) var5.next();
                 product.accept(this);
             }
 
@@ -323,21 +312,17 @@ public class HTMLExporterCustom extends HTMLExporter {
             featuresToVisit = this.nextFeaturesToVisit;
             this.nextFeaturesToVisit = new LinkedList();
             --this.featureDepth;
-            if(this.featureDepth >= 1) {
+            if (this.featureDepth >= 1) {
                 this.tr = table.appendElement("tr");
             }
         }
     }
 
     /**
-     *
      * @param args
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-/*
-
- */
         // Load a PCM
         File pcmFile = new File("pcms/PCM1/tesssvtttt369852147.pcm");
         //File paramFile = new File("pcms/PCM1/param1.json");
@@ -347,66 +332,36 @@ public class HTMLExporterCustom extends HTMLExporter {
         PCM pcm = loader.load(pcmFile).get(0).getPcm();
 
         HTMLExporterCustom te = new HTMLExporterCustom("PCM1/params1.json");
-        System.out.println(te.toHTML(pcm));
-        HTMLExporter testHtmlExporter = new HTMLExporter();
 
 
-        //modif by cheisda 24.11.2015
+        //Generate the HTML file
         generateHTMLFile(te, pcm);
-
-        try {
-
-            //TO DO : create a tmp folder
-            //Output file
-            FileOutputStream fos = new FileOutputStream("src\\"+System.currentTimeMillis()/1000+"TestArchivePDL.zip");
-            //Creating the output file
-            ZipOutputStream zos = new ZipOutputStream(fos);
-
-
-            //getting files to zip them
-            String fileGeneratedHTMLPath = "src\\HTMLGenerated.html";
-            String fileGeneratedCSSPath = "src\\style.css";
-            //getting Files size
-            int CSSSize = getFileSize(fileGeneratedCSSPath);
-            int HTMLSize = getFileSize(fileGeneratedHTMLPath);
-            System.out.println("Taille CSS : " + CSSSize + "octets, taille HTML : "+HTMLSize + "octets. ");
-            int totalFilesSize = CSSSize+HTMLSize;
-
-
-            //Adding to archive File
-            addToZipFile(fileGeneratedHTMLPath,zos, totalFilesSize);
-            addToZipFile(fileGeneratedCSSPath,zos,totalFilesSize);
-
-            //closing the streamsH
-            zos.close();
-            fos.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
+        //Generate the archive file which contains the CSS & HTML files
+        generateZIP();
 
     }//fin main
 
 
-    //Modif by Cheisda
-
     /**
+     * This function have 2 parameters, a PCM and the new data we want to use to generate the HTML file
+     * and the pcm which contains the products matrix
      *
      * @param dataResults
      * @param pcm
      */
     public static void generateHTMLFile(HTMLExporterCustom dataResults, PCM pcm) {
         try {
+            //create a new HTML file
             File HTMLGeneratedFile = new File("src\\HTMLGenerated.html");
+            //Write inside the HTML file
             FileWriter fileWriter = new FileWriter(HTMLGeneratedFile);
             fileWriter.write(dataResults.toHTML(pcm));
+            //Flucing and closing streams
             fileWriter.flush();
             fileWriter.close();
-            if (HTMLGeneratedFile == null){
+
+            //Checking wether the file is created
+            if (HTMLGeneratedFile == null) {
                 System.out.println("le fichier généré est vide");
             } else {
                 System.out.println("Bravo le fichier HTML a bien été généré !");
@@ -418,6 +373,9 @@ public class HTMLExporterCustom extends HTMLExporter {
     }
 
     /**
+     * Getting both files (CSS&HTML) size in order for the ZIP file to have the same size as the addition
+     * of both files sizes
+     * Return the size of a file
      *
      * @param filename
      * @return
@@ -428,42 +386,83 @@ public class HTMLExporterCustom extends HTMLExporter {
             System.out.println("File doesn\'t exist");
             return -1;
         }
-        return (int)file.length();
+        return (int) file.length();
     }
 
     /**
-     *
      * @param filePath
      * @param zos
      * @param filesSize
      * @throws FileNotFoundException
-     * @throws IOException
+     * @throws IOException           Adding file to the created ZIP file, it needs a output directory filePath, the zos parameter is the previous output ZIP file,
+     *                               and the fileSize is the size of the of the ZIP file we want to create
      */
-    private static void addToZipFile(String filePath, ZipOutputStream zos,int filesSize)throws FileNotFoundException,IOException {
+    private static void addToZipFile(String filePath, ZipOutputStream zos, int filesSize) throws FileNotFoundException, IOException {
         System.out.println("Writing '" + filePath + "' to zip file");
 
+        //Getting the concerned files
         File file = new File(filePath);
         FileInputStream fis = new FileInputStream(file);
+
+        //creating the zip file with the filePath
         ZipEntry zipEntry = new ZipEntry(filePath);
+
+        //implements an output stream filter for writing files in the ZIP file format
         zos.putNextEntry(zipEntry);
-
-
+        //defines the length of the ZIP file
         byte[] bytes = new byte[filesSize];
         int length;
         while ((length = fis.read(bytes)) >= 0) {
             zos.write(bytes, 0, length);
         }
 
+        //closing the streams
         zos.closeEntry();
         fis.close();
     }
 
+
     /**
-     *
+     * This function generate the archive file composed by HTMLGenerated.html and styles.css
+     * The styles.css has previously been generated by the CSSExporter class
+     */
+    public static void generateZIP() {
+        try {
+            //Output file with a different name each time it's generated
+            FileOutputStream fos = new FileOutputStream("src\\" + System.currentTimeMillis() / 1000 + "TestArchivePDL.zip");
+            //Creating the output file
+            ZipOutputStream zos = new ZipOutputStream(fos);
+
+            //getting files to zip them
+            String fileGeneratedHTMLPath = "src\\HTMLGenerated.html";
+            String fileGeneratedCSSPath = "src\\style.css";
+            //getting files size
+            int CSSSize = getFileSize(fileGeneratedCSSPath);
+            int HTMLSize = getFileSize(fileGeneratedHTMLPath);
+            //System.out.println("Taille CSS : " + CSSSize + "octets, taille HTML : "+HTMLSize + "octets. ");
+            int totalFilesSize = CSSSize + HTMLSize;
+
+            //Adding files to archive ZIP
+            addToZipFile(fileGeneratedHTMLPath, zos, totalFilesSize);
+            addToZipFile(fileGeneratedCSSPath, zos, totalFilesSize);
+
+            //closing the streams
+            zos.close();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
      * @param borneinf
      * @param bornesup
      * @param valuePCM
-     * @return
+     * @return This function defines the range of values the user wants to highlight
+     * the hilighted values are between parameters : borneinf ans bornsup
      */
     private boolean rangeIn(int borneinf, int bornesup, int valuePCM) {
         return ((valuePCM >= borneinf) && (valuePCM <= bornesup));
@@ -473,7 +472,8 @@ public class HTMLExporterCustom extends HTMLExporter {
      * @param borneinf
      * @param bornesup
      * @param valuePCM
-     * @return
+     * @return This function defines the range of values the user wants to highlight
+     * the highlighted values are not between parameters : borneinf ans bornsup
      */
     private boolean rangeOut(int borneinf, int bornesup, int valuePCM) {
         return ((valuePCM <= borneinf) || (valuePCM >= bornesup));
